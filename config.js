5dc38efa9c1239e4de2554bb5e07562e59682a18
realbaldiback <143491902+realbaldiback@users.noreply.github.com>
2023-08-29 11:15:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    clientId: "1145805422419263568", //Bot idsi
+    key: "", //ocr.space key link : https://ocr.space
+
+    //Alttakileri elleme !
+    langs: [
+        'Subscribed',
+        'Aboneolundu',
+        'Abunəoldu',
+        'Abone olundu',
+        'Abunə oldu',
+        'Gezeichnet',
+        'Abonné',
+        'Εγγεγραμμένος'
+    ],
+}
